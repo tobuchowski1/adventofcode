df8305d22dcb9ff8760e7457e706ec336bdc549b
Tomasz Obuchowski <t@obuchow.ski>
2025-12-29 02:24:16 +0100

    optimizations to 10

--- 2025/10_part2_gaussian.py
+++ 2025/10_part2_gaussian.py
@@ -41,6 +41,8 @@ def gauss(vectors: list[list[int]]) -> list[list[int]]:
                 # print(f"subtracting {i} times {times} from {j}")
                 matrix[j] = [x - times * y for x,y in zip(v,matrix[i])]
 
+
+    # breakpoint()
     # remove rows with all 0s
     matrix = [m for m in matrix if any(x != 0 for x in m)]
     return matrix
@@ -62,7 +64,7 @@ def guess_target(input_vectors: list[list[int]]) -> list[list[int]] | Failure:
     while isinstance(gaussian_result, Failure) and starting_sum <= max_reps:
         print(f"trying {starting_sum}")
         tv = ([1] * (len(input_vectors[0])-1)) + [starting_sum]
-        gaussian_result = resolve_variables(input_vectors + [tv], max_reps)
+        gaussian_result = resolve_variables(input_vectors + [tv], starting_sum)
         starting_sum += 1
     return gaussian_result
 
@@ -81,72 +83,102 @@ def resolve_variables(input_vectors: list[list[int]], max_reps: int) -> list[lis
         return resolve_variables(new_input, max_reps)
 
     gaussian = loop_gauss_till_halt(input_vectors)
-    # print("g", gaussian)
 
     if all([sum(v[:-1])==1 for v in gaussian]):
         if any(v[-1]<0 for v in gaussian):
             return Failure.NEGATIVE_VECTOR
         return gaussian
 
+    for v in gaussian:
+        if v[-1] < 0 and all(x>0 for x in v[:-1]):
+            return Failure.NEGATIVE_VECTOR
+
     best_gaussian = Failure.NOT_FOUND
     best_sum = 1000000000
-    for v in gaussian:
-        ones_cols = [i for i,y in enumerate(v[:-1]) if y == 1]
-        if len([x for x in v[:-1] if x !=0]) > 1 and v[-1] > 0 and ones_cols:
-            idx = ones_cols[0]
-            # if v[idx] != 1:
-            #     continue
-            # lo = 0
-            # hi = v[-1]
-            # value_cache = {}
-            #
-            # while lo < hi:
-            #     mid = (lo + hi) // 2
-            #
-            #     if mid not in value_cache:
-            #         value_cache[mid] = bind_var(idx, mid, v)
-            #
-            #     if (mid+1) not in value_cache:
-            #         value_cache[mid+1] = bind_var(idx, mid + 1, v)
-            #
-            #     if value_cache[mid + 1] is Failure.NEGATIVE_VECTOR:
-            #         hi = mid
-            #         continue
-            #
-            #     if value_cache[mid] is Failure.NOT_FOUND:
-            #         lo = mid + 1
-            #         continue
-            #
-            #     if isinstance(value_cache[mid], Failure) or isinstance(value_cache[mid+1], Failure):
-            #         breakpoint()
-            #
-            #     if value_cache[mid] > value_cache[mid + 1]:
-            #         lo = mid + 1
-            #     else:
-            #         hi = mid
-            #
-            # return value_cache[lo]
 
-            # range(v[-1]+1)
-            max_val = v[-1]+1
+    bindings = []
+    for v_idx,v in enumerate(gaussian):
+        ones_cols = [i for i,y in enumerate(v[:-1]) if y == 1]
+        non_zero_cols = len([x for x in v[:-1] if x !=0])
+        if non_zero_cols == 0 and v[-1] != 0:
+            return Failure.NOT_FOUND
+        if non_zero_cols > 1 and v[-1] > 0 and ones_cols:
+            idx = ones_cols[0]
+            max_val = v[-1] + 1
             if any([x < 0 for x in v[:-1]]):
                 max_val = max_reps + 1
-            for val in range(max_val)[::-1]:
-                nresult = bind_var(idx, val, v)
-                if not isinstance(nresult, Failure):
-                    # return nresult
-                    nsum = sum([v[-1] for v in nresult])
-                    # if idx == 1:
-                    # print('val,nsum', val,nsum)
+            bindings.append((max_val, idx, v_idx))
 
-                    if best_sum > nsum:
-                        best_sum = nsum
-                        best_gaussian = nresult
-                    else:
-                        break
-                # elif idx == 1:
-                #     print(val,nresult)
-            break
+    if len(bindings) > 0:
+        bindings.sort(key=lambda x: x[0])
+        max_val, vv_idx, v_idx = bindings[0]
+        for val in range(max_val)[::-1]:
+            nresult = bind_var(vv_idx, val, gaussian[v_idx])
+            if not isinstance(nresult, Failure):
+                nsum = sum([v[-1] for v in nresult])
+                if best_sum > nsum:
+                    best_sum = nsum
+                    best_gaussian = nresult
+                else:
+                    break
+
+    # for v in gaussian:
+    #     ones_cols = [i for i,y in enumerate(v[:-1]) if y == 1]
+    #     if len([x for x in v[:-1] if x !=0]) > 1 and v[-1] > 0 and ones_cols:
+    #         idx = ones_cols[0]
+    #         # if v[idx] != 1:
+    #         #     continue
+    #         # lo = 0
+    #         # hi = v[-1]
+    #         # value_cache = {}
+    #         #
+    #         # while lo < hi:
+    #         #     mid = (lo + hi) // 2
+    #         #
+    #         #     if mid not in value_cache:
+    #         #         value_cache[mid] = bind_var(idx, mid, v)
+    #         #
+    #         #     if (mid+1) not in value_cache:
+    #         #         value_cache[mid+1] = bind_var(idx, mid + 1, v)
+    #         #
+    #         #     if value_cache[mid + 1] is Failure.NEGATIVE_VECTOR:
+    #         #         hi = mid
+    #         #         continue
+    #         #
+    #         #     if value_cache[mid] is Failure.NOT_FOUND:
+    #         #         lo = mid + 1
+    #         #         continue
+    #         #
+    #         #     if isinstance(value_cache[mid], Failure) or isinstance(value_cache[mid+1], Failure):
+    #         #         breakpoint()
+    #         #
+    #         #     if value_cache[mid] > value_cache[mid + 1]:
+    #         #         lo = mid + 1
+    #         #     else:
+    #         #         hi = mid
+    #         #
+    #         # return value_cache[lo]
+    #
+    #         # range(v[-1]+1)
+    #         max_val = v[-1]+1
+    #         if any([x < 0 for x in v[:-1]]):
+    #             max_val = max_reps + 1
+    #         for val in range(max_val)[::-1]:
+    #             nresult = bind_var(idx, val, v)
+    #             if not isinstance(nresult, Failure):
+    #                 # return nresult
+    #                 nsum = sum([v[-1] for v in nresult])
+    #                 # if idx == 1:
+    #                 # print('val,nsum', val,nsum)
+    #
+    #                 if best_sum > nsum:
+    #                     best_sum = nsum
+    #                     best_gaussian = nresult
+    #                 else:
+    #                     break
+    #             # elif idx == 1:
+    #             #     print(val,nresult)
+    #         break
 
     return best_gaussian
 
@@ -168,6 +200,11 @@ l12 = ["[##....#...] (2,3,4,5,6,7) (0,1,2,3,4,6,9) (0,1,3,4,6,8,9) (1,2,3,4,6) (
 l13 = ["[##...#....] (1,3,9) (0,1,2,3,4,7,8,9) (1,4,8) (3,5,8,9) (0,1,3,5,7,8,9) (0,2,4,6,7,8) (1,2,4,5,6,7,8,9) (0,6,8,9) (2,6) (0,2,6,8,9) (9) {38,32,35,25,17,22,46,28,49,53}"]
 l14 = ["[##.####] (0,2,5,6) (4) (0,2,4,6) (2,3,4,5) (0,1) (1,3,5) (3,4,5,6) (2,6) (0,3,4,5,6) {45,15,30,53,58,67,56} ?87?"]
 
+l15 = ["[.##..] (1,4) (1,2,3,4) (2,3) (0,3,4) (0,4) (3,4) (0,1,2,3) {25,22,8,43,60} ?60?"]
+lslow = ["[##.##.....] (1,3,7,8) (1,3,5,8) (0,2,3,4,5,6,8) (8) (0,1,2,3,5,7,8,9) (0,2,6,7) (4,6,7) (0,1,7,8,9) (2,3,5,8,9) (0,4,5,6) (1,4,5,9) (2,3,4,6,8,9) (2,4,6,8,9) {220,54,231,228,238,235,238,54,267,70}"]
+
+
+# for line in lslow:
 for line in sys.stdin:
     line = line.strip()
     data = line.split(" ")
@@ -205,6 +242,7 @@ for line in sys.stdin:
     # print(vectors)
 
     gaussian = guess_target(vectors)
+    # gaussian = resolve_variables(vectors, sum([v[-1] for v in vectors]))
 
     # print("b", buttons)
     # print(gaussian)
